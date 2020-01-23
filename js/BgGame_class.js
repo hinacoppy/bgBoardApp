cb7eb1be630e4939de66895e4f57f8c5dc67f007
hinacoppy <kawakami@js4.so-net.ne.jp>
2020-01-23 12:04:47 +0900

    re-draw board when orientation changed (for iOS)

--- js/BgGame_class.js
+++ js/BgGame_class.js
@@ -117,6 +117,10 @@ class BgGame {
       this.settingbtn.prop("disabled", false);
     });
 
+    if (BgUtil.isIOS()) {
+      window.addEventListener("orientationchange", () => { this.board.showBoard2(this.xgid); }); 
+    }
+
   }
 
   initGameOption() {
